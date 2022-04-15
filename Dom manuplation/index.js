2a75cdf5e0208e3bd6ba220e205c2f027513bca8
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-04-15 20:03:23 +0530

    Dom dir completed

---
+++ Dom manuplation/index.js
@@ -0,0 +1,9 @@
+var button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click",function(){
+
+    console.log("Click")
+})
+
+document.querySelector("h2").style.background="blue";
+document.querySelector("h3").style.background="green"; 
