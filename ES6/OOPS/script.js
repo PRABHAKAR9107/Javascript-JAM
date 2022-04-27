a7aa14fe85af57787444767208ccc163d76298e2
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-04-27 23:55:46 +0530

    add oops dir

--- ES6/OOPS/script.js
+++ ES6/OOPS/script.js
@@ -34,3 +34,41 @@ console.log(car1, car2, car3);
 
 //Constructors have the same name as that of the object which is being created.
 // As a convention, the first alphabet is kept capital in the constructor function.
+
+//Object Prototype
+//The prototype property allows you to add new methods to objects constructors:
+
+let obj = {
+  name: "harry",
+  channel: "Code With Harry",
+  address: "Mars",
+};
+
+function Obj(givenName) {
+  this.name = givenName;
+}
+
+Obj.prototype.givenAge = 25;
+Obj.prototype.getName = function () {
+  return this.name;
+};
+
+Obj.prototype.setName = function (newName) {
+  this.name = newName;
+};
+
+let obj2 = new Obj("Rohan Das");
+console.log(obj2);
+console.log(obj2.givenAge); //25
+
+function Student() {
+  this.name = "Harry";
+  this.gender = "Male";
+}
+Student.prototype.sayHi = function () {
+  console.log("Hello World!");
+};
+let std = new Student();
+
+console.log(std);
+console.log(std.toString()); //[object Object]
