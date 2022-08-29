780e01050e91b7097a295fa20aadcedf530a204b
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-08-29 14:56:33 +0530

    modules added

---
+++ ES6/Modules/greeting.js
@@ -0,0 +1,13 @@
+export const formalGreeting = (person) => {
+  console.log(`GoodMorning! ${person}`);
+};
+
+export const informalGreeting = (person) => {
+  console.log(`Hello! ${person}`);
+};
+
+const greeting = () => {
+  console.log("Hello World!");
+};
+
+export default greeting;
